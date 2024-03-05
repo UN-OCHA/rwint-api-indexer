65dbb362365a9fdde7250b9b768cad4522da5a70
orakili <github@orakili.net>
2024-03-05 04:43:07 +0000

    feat: index UUID
    Refs: RW-867

--- src/RWAPIIndexer/Resources/Job.php
+++ src/RWAPIIndexer/Resources/Job.php
@@ -2,8 +2,8 @@
 
 namespace RWAPIIndexer\Resources;
 
-use RWAPIIndexer\Resource;
 use RWAPIIndexer\Mapping;
+use RWAPIIndexer\Resource;
 
 /**
  * Job resource handler.
@@ -95,6 +95,7 @@ class Job extends Resource {
   public function getMapping() {
     $mapping = new Mapping();
     $mapping->addInteger('id')
+      ->addString('uuid', FALSE)
       ->addString('url', FALSE)
       ->addString('url_alias', FALSE)
       ->addString('redirects', FALSE)
--- src/RWAPIIndexer/Resources/Topic.php
+++ src/RWAPIIndexer/Resources/Topic.php
@@ -2,8 +2,8 @@
 
 namespace RWAPIIndexer\Resources;
 
-use RWAPIIndexer\Resource;
 use RWAPIIndexer\Mapping;
+use RWAPIIndexer\Resource;
 
 /**
  * Topic resource handler.
@@ -86,6 +86,7 @@ class Topic extends Resource {
   public function getMapping() {
     $mapping = new Mapping();
     $mapping->addInteger('id')
+      ->addString('uuid', FALSE)
       ->addString('url', FALSE)
       ->addString('url_alias', FALSE)
       ->addString('redirects', FALSE)
